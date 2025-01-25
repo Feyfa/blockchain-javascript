2dde3f26a3fd7f129a3977ddb8e154fa73f8ab0b
Feyfa <fisikamodern00@gmail.com>
2025-01-25 18:06:24 +0700

    learn proof of work blockchain

--- dev/blockchain.mjs
+++ dev/blockchain.mjs
@@ -1,3 +1,5 @@
+import sha256 from "sha256";
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransaction = [];
@@ -33,6 +35,32 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
     this.pendingTransaction.push(newTransaction);
 
     return this.getLastBlock().index + 1;
+}
+
+Blockchain.prototype.hashBlock = function (previousBlockHash, currentBlockData, nonce) {
+    const data = `${previousBlockHash}${nonce.toString()}${JSON.stringify(currentBlockData)}`;
+    const hash = sha256(data);
+    return hash;
+}
+
+Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData) {
+    // bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    // 1. repeatedly has block until itu find correct hash => '00000IANSYHSBSYG'
+    // 2. uses current block data for the hash, but also the previousBlockHash
+    // 3. continuously changes nonce value until it finds the correct hash
+    // 4. retrurns to us the nonce value that creates the correct hash
+
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0,4) != '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    }
+    
+    return {
+        nonce: nonce,
+        hash: hash
+    };
 }
 
 export default Blockchain;
--- dev/test.mjs
+++ dev/test.mjs
@@ -2,17 +2,48 @@ import Blockchain from "./blockchain.mjs";
 
 const bitcoin = new Blockchain();
 
-// Tambang Block Dan Buat Transaction
-bitcoin.createNewBlock(892348, 'ASDFGH', 'QWERTY');
-bitcoin.createNewTransaction(100, 'JED12345', 'FEYFA123456');
+/* --------------------------------------------- */
+// // Tambang Block Dan Buat Transaction
+// bitcoin.createNewBlock(892348, 'ASDFGH', 'QWERTY');
+// bitcoin.createNewTransaction(100, 'JED12345', 'FEYFA123456');
 
-// Tambang Block Dan Buat Transaction
-bitcoin.createNewBlock(123123, 'JLKNSWDD', 'PQSDDHJ');
-bitcoin.createNewTransaction(10, 'JED12345', 'FEYFA123456');
-bitcoin.createNewTransaction(20, 'JED12345', 'FEYFA123456');
-bitcoin.createNewTransaction(50, 'JED12345', 'FEYFA123456');
+// // Tambang Block Dan Buat Transaction
+// bitcoin.createNewBlock(123123, 'JLKNSWDD', 'PQSDDHJ');
+// bitcoin.createNewTransaction(10, 'JED12345', 'FEYFA123456');
+// bitcoin.createNewTransaction(20, 'JED12345', 'FEYFA123456');
+// bitcoin.createNewTransaction(50, 'JED12345', 'FEYFA123456');
 
-// Tambang Block Dan Buat Transaction
-bitcoin.createNewBlock(1243531, 'ASfahgs127', 'AHJKBAHKBV7');
+// // Tambang Block Dan Buat Transaction
+// bitcoin.createNewBlock(1243531, 'ASfahgs127', 'AHJKBAHKBV7');
 
-console.log(bitcoin);
+// console.log(bitcoin);
+/* --------------------------------------------- */
+
+
+
+/* --------------------------------------------- */
+const previousBlockHash = 'KSA1231SAQWKDLSSA';
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'JHKSKJHS778A',
+        recipient: 'KJHKV786HJKV'
+    },
+    {
+        amount: 30,
+        sender: 'JHKSKJHS778',
+        recipient: 'NCVNCBS918'
+    },
+    {
+        amount: 50,
+        sender: 'ASWWEQEQW33541',
+        recipient: 'MUHYHGTGATA515'
+    },
+];
+
+// process untuk match 4 character di awal hash
+console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockData));
+
+// pembuktian dengan nonce jika proofOfWork berhasil dengan nonce yang dihasilkan
+// console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 31374));
+/* --------------------------------------------- */
