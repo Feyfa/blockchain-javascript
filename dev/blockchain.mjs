cc2df875102b8fc644ce38f3c7da02558c13536a
Feyfa <fisikamodern00@gmail.com>
2025-02-16 21:38:15 +0700

    create method for check chain valid or not

--- dev/blockchain.mjs
+++ dev/blockchain.mjs
@@ -73,4 +73,39 @@ Blockchain.prototype.proofOfWork = function (previousBlockHash, currentBlockData
     return nonce;
 }
 
+Blockchain.prototype.chainIsValid = function (blockchain) {
+    let validChain = true;
+
+    for(let i = 1; i < blockchain.length; i++) {
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i - 1];
+
+        const currentBlockData = {
+            transaction: currentBlock.transaction,
+            index: currentBlock.index
+        }
+        const blockHash = this.hashBlock(prevBlock.hash, currentBlockData, currentBlock.nonce);
+
+        if(blockHash.substring(0, 4) !== '0000') {
+            validChain = false;
+        }
+        if(currentBlock.previousBlockHash !==  prevBlock.hash) { 
+            validChain = false;
+        }
+    }
+
+    // genesis block adalah block paling awal
+    const genesisBlock = blockchain[0];
+    const correctNonce = (genesisBlock.nonce === 100);
+    const correctPreviousBlockHash = (genesisBlock.hash === '0');
+    const correctHash = (genesisBlock.hash === '0');
+    const correctTransactions = (genesisBlock.transaction.length === 0);
+
+    if(!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) {
+        validChain = false;
+    }
+
+    return validChain;
+}
+
 export default Blockchain;
--- dev/test.mjs
+++ dev/test.mjs
@@ -51,6 +51,134 @@ const bitcoin = new Blockchain();
 
 
 /* --------------------------------------------- */
-console.log(bitcoin);
+// Testing for chain is valid
+const bc1 = {
+    "chain": [
+        {
+            "index": 1,
+            "timestamp": 1739714584966,
+            "transaction": [],
+            "nonce": 100,
+            "hash": "0",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 2,
+            "timestamp": 1739714591537,
+            "transaction": [],
+            "nonce": 16441,
+            "hash": "00009b2ef664890dbcd795344f8145bac1710db47cea457183f41c9ca24c3285",
+            "previousBlockHash": "0"
+        },
+        {
+            "index": 3,
+            "timestamp": 1739714623334,
+            "transaction": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "f670dd94b2ad45709257b3b31e664aea",
+                    "transactionId": "68c158de9e97469a9b8b7cacff26fd99"
+                },
+                {
+                    "amount": 30,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "764062ad511b4079bc9f3a967e986722"
+                },
+                {
+                    "amount": 10,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "d9c2c593b73444f4888c958f7172abef"
+                }
+            ],
+            "nonce": 21672,
+            "hash": "0000fdc10f3526538fac195e9a64a1b90c54eb0b760c422954ead5f5d1105d76",
+            "previousBlockHash": "00009b2ef664890dbcd795344f8145bac1710db47cea457183f41c9ca24c3285"
+        },
+        {
+            "index": 4,
+            "timestamp": 1739714719024,
+            "transaction": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "f670dd94b2ad45709257b3b31e664aea",
+                    "transactionId": "69899a392ef64e5da73e1a8b16baca86"
+                },
+                {
+                    "amount": 40,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "12edbf39a71b4bb48aa345c8cfa6939a"
+                },
+                {
+                    "amount": 50,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "e044a16caab749ee8c16a6563c0377ac"
+                },
+                {
+                    "amount": 60,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "a84f1816fadc42a3b4cb6f58edb50894"
+                },
+                {
+                    "amount": 70,
+                    "sender": "JIDAN123",
+                    "recipient": "FEYFA123",
+                    "transactionId": "459b7e03b0754f93a9acf6d584e7d25d"
+                }
+            ],
+            "nonce": 87728,
+            "hash": "000031df4ab5c21bfdeda9a048b98e9ce9c89d2300b50d89e0347e985f23dccc",
+            "previousBlockHash": "0000fdc10f3526538fac195e9a64a1b90c54eb0b760c422954ead5f5d1105d76"
+        },
+        {
+            "index": 5,
+            "timestamp": 1739714763038,
+            "transaction": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "f670dd94b2ad45709257b3b31e664aea",
+                    "transactionId": "da7fe02660d94349981a47157f4afa61"
+                }
+            ],
+            "nonce": 22423,
+            "hash": "0000c826ce21de05f074f63ed070cd49bfe4b197c062a3ea7f1cf6d01446193e",
+            "previousBlockHash": "000031df4ab5c21bfdeda9a048b98e9ce9c89d2300b50d89e0347e985f23dccc"
+        },
+        {
+            "index": 6,
+            "timestamp": 1739714764788,
+            "transaction": [
+                {
+                    "amount": 12.5,
+                    "sender": "00",
+                    "recipient": "f670dd94b2ad45709257b3b31e664aea",
+                    "transactionId": "9b331511eb0147cdb058cc4522ca975f"
+                }
+            ],
+            "nonce": 8364,
+            "hash": "0000580c05459bf1cd76be4fdf1fbb001286970f1fcc330ab3c2bc5e4d6d994b",
+            "previousBlockHash": "0000c826ce21de05f074f63ed070cd49bfe4b197c062a3ea7f1cf6d01446193e"
+        }
+    ],
+    "pendingTransaction": [
+        {
+            "amount": 12.5,
+            "sender": "00",
+            "recipient": "f670dd94b2ad45709257b3b31e664aea",
+            "transactionId": "eeb0d73227544d8588d142941936931e"
+        }
+    ],
+    "currentNodeUrl": "http://localhost:3001",
+    "networkNodes": []
+}
+
+console.log(`VALID : ${bitcoin.chainIsValid(bc1.chain)}`);
 /* --------------------------------------------- */
 
